251765bd8dff6b7a6dc38ca2719871278d3d95f8
Stanislav1275 <aristovstas2004@gmail.com>
2025-06-24 15:48:53 +0400

    feat: global css and meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,32 @@
-<script>
+<script lang="ts">
+	import '$lib/styles/global.css';
+
+	//todo deprecate
+	import { page } from '$app/stores';
+
 	/**
 	 * Components
 	 */
-	import Footer from '@pages/Footer.svelte';
+	$: ({ url } = $page);
 </script>
 
-<div class="layout">
+<svelte:head>
+	<!-- Basic Meta Tags -->
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="theme-color" content="#ffffff" />
+
+	<title>Blog | Latest Posts</title>
+	<meta name="description" content="Read posts everyday @aristovstas2004" />
+
+	<meta content="Blog" property="og:site_name" />
+	<meta content={url.href} property="og:url" />
+	<meta content="website" property="og:type" />
+</svelte:head>
+
+<main>
 	<slot></slot>
-	<Footer />
-</div>
+</main>
 
 <style lang="scss">
   .layout {
@@ -25,5 +43,12 @@
 
     width: 100%;
     height: max-content;
+  }
+
+  main {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 2rem;
+    min-height: 100vh;
   }
 </style>
